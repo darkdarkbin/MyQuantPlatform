3d46c8e5781a7480735cc0e466478739c73b28a1
darkdarkbin <darkdarkbin@gmail.com>
2026-07-19 23:44:53 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+/*
+ * Firebase Console > 프로젝트 설정 > 일반 > 내 앱 > SDK 설정 및 구성에서
+ * firebaseConfig 값을 복사해 아래 따옴표 안에 붙여넣으세요.
+ *
+ * 이 설정은 Firebase 프로젝트를 식별하는 공개 웹 설정입니다.
+ * 실제 사용자 데이터 접근은 firestore.rules와 Google 로그인으로 보호합니다.
+ */
+window.MQP_FIREBASE_CONFIG = Object.freeze({
+  apiKey: "AIzaSyBTXL7pFjLByBmAnaK929vOHECLzS4A_F8",
+  authDomain: "myquantplatform-825f9.firebaseapp.com",
+  projectId: "myquantplatform-825f9",
+  storageBucket: "myquantplatform-825f9.firebasestorage.app",
+  messagingSenderId: "255845974189",
+  appId: "1:255845974189:web:1fa778e8e6e31d136d8624",
+  measurementId: "G-REVIEWYZ11"
+});
